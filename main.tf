13c167e27fe507effc495e11d62d4f0782f85835
dharmendra-vervotech <58485463+dharmendra-vervotech@users.noreply.github.com>
2020-05-09 23:39:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 module "s3-webapp" {
   //source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
   source  = "app.terraform.io/vervotech/s3-webapp/aws"
+  version = "0.0.1"
   name   = var.name
   region = var.region
   prefix = var.prefix
